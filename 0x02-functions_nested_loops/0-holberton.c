205a48154f3c81abcb15fa7cee7cd4778a222574
VendettaPrince22 <pomegah@gmail.com>
2021-09-01 05:04:30 +0000

    task 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 /**
- * main - prints the string "Holberton" from a character array.
+ * main - prints the string "_putchar" from a character array.
  * Return: 0 on success.
  */
 int main(void)
 {
-char c[] = "Holberton";
+char c[] = "_putchar";
 int var;
 
 for (var = 0; var <= 8; var++)
