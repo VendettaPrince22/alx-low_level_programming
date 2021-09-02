2d6866e247693fc1677205e07f0d15dc78f261e2
VendettaPrince22 <pomegah@gmail.com>
2021-09-02 13:08:19 +0000

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print from 0 - 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+int num;
+
+num = 48;
+while (num <= 57)
+{
+_putchar(num);
+num++;
+}
+_putchar('\n');
+}
