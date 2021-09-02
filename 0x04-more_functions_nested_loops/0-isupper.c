e099034b63f82e4b2a79fe9fe4b7fa39f2b6395f
VendettaPrince22 <pomegah@gmail.com>
2021-09-02 12:37:43 +0000

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,11 @@
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-		{
-		return (1);
-		}
-		else
-		{
-		return (0);
-		}
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
