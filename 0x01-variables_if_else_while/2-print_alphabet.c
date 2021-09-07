71b00d91165d89375519f00179ec50c53eac7dbc
VendettaPrince22 <pomegah@gmail.com>
2021-09-07 09:07:19 +0000

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,22 +6,10 @@
  */
 int main(void)
 {
-int n, lastdigit;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-lastdigit = n % 10;
-if (lastdigit > 5)
-{
-printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
-}
-else if (lastdigit == 0)
-{
-printf("Last digit of %d is %d and is 0\n", n, lastdigit);
-}
-else if (lastdigit < 6 && lastdigit != 0)
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
-}
+char var;
+
+for (var = 'a'; var <= 'z'; var++)
+putchar(var);
+putchar('\n');
 return (0);
 }
