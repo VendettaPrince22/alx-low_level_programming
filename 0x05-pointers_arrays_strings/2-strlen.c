605025c2d5b8b67fbd407c8ee56c32df855b87e3
VendettaPrince22 <pomegah@gmail.com>
2021-09-07 06:40:49 +0000

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns length of a string
+ * @s: pointer type char
+ * Return: program will return number of characters of *s
+ */
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0; s[i] != 00; i++)
+{
+}
+return (i);
+}
