01c01936d218ca081b9086e6ac6d5f16918a2344
VendettaPrince22 <pomegah@gmail.com>
2021-09-07 07:08:38 +0000

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s: string
+ */
+void rev_string(char *s)
+{
+int i;
+int j;
+char tmp;
+
+for (i = 0; s[i] != 00; i++)
+{
+}
+i = i - 1;
+
+for (j = 0; j <= i; j++)
+{
+tmp = s[j];
+s[j] = s[i];
+s[i] = tmp;
+i--;
+}
+}
