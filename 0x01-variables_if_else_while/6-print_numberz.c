e90ac158fa81c06592b53475f5ded353dd522060
VendettaPrince22 <pomegah@gmail.com>
2021-09-07 09:37:35 +0000

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int number = 48;
+while (number <= 57)
+{
+putchar(number);
+number++;
+}
+putchar('\n');
+return (0);
+}
