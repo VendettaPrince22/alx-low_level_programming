2f675ff20778a8f4ecd09c8335ff3931c91ebfc3
VendettaPrince22 <pomegah@gmail.com>
2021-09-07 07:48:27 +0000

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half print half of a string
+ * @str: passing pointer
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int length_of_the_string = 0;
+
+while (*str != '\0')
+{
+length_of_the_string++;
+str++;
+}
+if ((length_of_the_string % 2) == 0)
+{
+str -= (length_of_the_string / 2);
+}
+else
+{
+str -= ((length_of_the_string - 1) / 2);
+}
+
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
