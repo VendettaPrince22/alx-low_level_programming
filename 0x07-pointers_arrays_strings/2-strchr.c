45f391e1cc6b3dbbd74e3dce907b1a99ccc6ff33
VendettaPrince22 <pomegah@gmail.com>
2021-09-14 17:45:51 +0000

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strchr - search a character in a string
+ * @s: string
+ * @c: character to find
+ * Return: s + i
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+
+for (i = 0; s[i] != 00; i++)
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+if (s[i + 1] == c);
+{
+return (s + i + 1);
+}
+}
+return (0);
+}
