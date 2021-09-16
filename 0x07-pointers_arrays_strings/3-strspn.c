76e0ae7db1c272c13df3f8fa9a646ae7e239c95b
VendettaPrince22 <pomegah@gmail.com>
2021-09-16 06:52:34 +0000

    pointers 1

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strspn - get the length of a prefix substring
+ * @s: string
+ * @accept: consecutive search
+ * Return: occurences found
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j, n;
+
+n = 0;
+
+for (i = 0; s[i] != 00; i++)
+{
+for (j = 0; accept[j] != 00; j++)
+{
+if (s[i] == accept[j])
+{
+n++;
+break;
+}
+}
+if (!accept[j])
+{
+break;
+}
+}
+return (n);
+}
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - search 's'
+ * @s: string
+ * @accept: search
+ * Return: char found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+
+for (; *s; s++)
+{
+for (i = 0; accept[i]; i++)
+{
+if (*s == accept[i])
+{
+return (s);
+}
+}
+}
+return (0);
+}
