85c13e98c36485ad8962bc5e389a94a0c7a431df
VendettaPrince22 <pomegah@gmail.com>
2021-11-15 09:20:03 +0000

    5 Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
  * main - entry point
  *
  * Return: Always 0 (Success)
- */
+ **/
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return 0;
+	printf("with proper grammer, but the outcome is a piece of art, \n");
+	return (0);
 }
