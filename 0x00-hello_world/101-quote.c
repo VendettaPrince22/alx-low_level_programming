58507dc73048ff002bf34adda0238e9e4a4daa69
VendettaPrince22 <pomegah@gmail.com>
2021-11-16 09:14:10 +0000

    8 UNIX is basically a simple OS

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 1 (success)
  */
 int main(void)
 {
 	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	return (1);
 }
