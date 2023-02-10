486a266f3cd10eeac125b45ede31bea59008ed56
Nashy Krell <gamerific9000@outlook.com>
2023-02-10 10:09:43 +0300

    Fixing it up

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	char s[] = "Programming is like building a multilingual puzzle";
+	puts(s);
+}
