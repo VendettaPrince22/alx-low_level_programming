17178c82ea80f22ae3169b2c8a45dcaf06b334d1
Kairo Prince <pomegah@gmail.com>
2024-05-12 02:40:46 -0700

    just standard stuff

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print string to stdout
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+}
