5fc7b923f1ec0467a7001b9fbe876df54f4c6350
Kairo Prince <pomegah@gmail.com>
2024-06-11 12:56:25 -0400

    Returns number of elements in a dllist

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - number of elements in a linked list
+ * @h: pointer to head of dllist
+ *
+ * Returns: number of elements (size_t)
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n;
+
+	n = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+}
