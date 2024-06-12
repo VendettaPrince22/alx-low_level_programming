36cd79e05762d8102806b8c7a30e1cfc2f5e1b1e
Kairo Prince <pomegah@gmail.com>
2024-06-11 23:54:47 -0400

    Adds a new node at end of a dll

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,6 +15,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *current;
 
 	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 
 	new->n = n;
 
