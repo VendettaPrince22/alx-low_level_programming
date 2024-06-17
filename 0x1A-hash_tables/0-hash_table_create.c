00ce54062a66b59e80a4cb05d049f50181210a73
Kairo Prince <pomegah@gmail.com>
2024-06-17 12:18:50 -0400

    Creating a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,12 +11,23 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
+	hash_node_t **array_new;
 
-	new_table = malloc(size);
+	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 	{
 		return (NULL);
 	}
+	array_new = malloc(sizeof(hash_node_t) * size);
+	if (array_new == NULL)
+	{
+		return (NULL);
+	}
+
+	*array_new = NULL;
+
+	new_table->size = size;
+	new_table->array = array_new;
 
 	return (new_table);
 }
