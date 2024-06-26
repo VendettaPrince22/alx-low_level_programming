e24491af99d27e26b5c6cd0c6f8bd3bb4ba58ad1
Kairo Prince <pomegah@gmail.com>
2024-06-26 09:40:34 -0400

    Copies memory area

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: buffer to print to
+ * @b: byte to print
+ * @n: number of bytes to fill
+ *
+ * Return: Pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+        char *str;
+        unsigned int i;
+
+        i = 0;
+        while (i < n)
+        {
+                s[i] = b;
+                i++;
+        }
+        str = s;
+
+        return (str);
+}
