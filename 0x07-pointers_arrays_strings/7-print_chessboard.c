febc9f69d62828db44d655cfda395a9293cd1b9e
Kairo Prince <pomegah@gmail.com>
2024-06-27 04:13:14 -0400

    Prints a chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: array of arrays to print
+ */
+void print_chessboard(char (*a)[8])
+{
+        int i;
+        int j;
+        char k;
+
+        i = 0;
+        while (i < 8)
+        {
+                j = 0;
+                while (j < 8)
+                {
+                        k = a[i][j];
+                        _putchar(k);
+                        j++;
+                }
+                _putchar('\n');
+                i++;
+        }
+}
