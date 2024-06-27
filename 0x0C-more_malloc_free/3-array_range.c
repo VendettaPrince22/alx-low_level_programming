15ad32b75401ea68395934f1bb09f3e8c48780a9
Kairo Prince <pomegah@gmail.com>
2024-06-27 08:47:26 -0400

    Create an array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,42 +2,42 @@
 
 /**
  * array_range - creates an array of integers
- * 
+ *
  * @min: minimum integer; included
  * @max: maximum integer; included
- * 
+ *
  * Return: pointer to new array
  * if min greater than max, return NULL
  * On fail, return NULL
  */
 int *array_range(int min, int max)
 {
-    int *pti;
-    int n;
-    int i;
-    int nums;
+	int *pti;
+	int n;
+	int i;
+	int nums;
 
-    /*Check if max is less than min*/
-    if (min > max)
-    {
-        return NULL;
-    }
+	/*Check if max is less than min*/
+	if (min > max)
+	{
+		return (NULL);
+	}
 
-    /*Get size of the array / difference between min and max both inclusive*/
-    n = (max - min) + 1;
-    pti = malloc(n * sizeof(*pti));
+	/*Get size of the array / difference between min and max both inclusive*/
+	n = (max - min) + 1;
+	pti = malloc(n * sizeof(*pti));
 
-    /*Allocate the numbers into the array*/
-    for (i = 0, nums = min; i < n; i++, nums++)
-    {
-        pti[i] = nums;
-    }
+	/*Allocate the numbers into the array*/
+	for (i = 0, nums = min; i < n; i++, nums++)
+	{
+		pti[i] = nums;
+	}
 
-    /*On fail*/
-    if (pti == NULL)
-    {
-        return NULL;
-    }
+	/*On fail*/
+	if (pti == NULL)
+	{
+		return (NULL);
+	}
 
-    return (pti);
+	return (pti);
 }
