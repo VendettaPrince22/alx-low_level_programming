3e27819bb6185b78ab6d63ec9a5114ac3f556945
Kairo Prince <pomegah@gmail.com>
2024-07-01 08:12:20 -0400

    Reads a textfile and prints to POSIX stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,7 +8,7 @@
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: name of the file
  * @letters: number of letters it should read and print
- * 
+ *
  * Return: actual number of letters it could read and print
  * if file cannot be opened or read, return 0
  * if filename is NULL, return 0
@@ -16,40 +16,40 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        ssize_t byts;
-        int fd;
-        char *buf;
-        ssize_t out;
+	ssize_t byts;
+	int fd;
+	char *buf;
+	ssize_t out;
 
-        if (filename == NULL)
-        {
-                return (0);
-        }
+	if (filename == NULL)
+	{
+		return (0);
+	}
 
-        fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-        if (fd == -1)
-        {
-                return (0);
-        }
+	if (fd == -1)
+	{
+		return (0);
+	}
 
-        buf = malloc(letters * sizeof(char));
-        byts = read(fd, buf, letters);
+	buf = malloc(letters * sizeof(char));
+	byts = read(fd, buf, letters);
 
-        if (byts == -1)
-        {
-                return (0);
-        }
+	if (byts == -1)
+	{
+		return (0);
+	}
 
-        out = write(STDOUT_FILENO, buf, letters);
+	out = write(STDOUT_FILENO, buf, letters);
 
-        if (out == -1)
-        {
-                return (0);
-        }
-        
-        free(buf);
-        close(fd);
+	if (out == -1)
+	{
+		return (0);
+	}
 
-        return (byts);
+	free(buf);
+	close(fd);
+
+	return (byts);
 }
