91a0f351ee652fda7798764ee1d88915fd7999b2
Kairo Prince <pomegah@gmail.com>
2024-07-03 01:20:31 -0400

    Insert an element to hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table to add to or update
+ * @key: the key to add; cannot be empty
+ * @value: the value associated  with the key; must be duplicated, can be empty
+ *
+ * Return: 1 on success; 0 on fail
+ * In case of collision, add new node at beginning of the list
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+        hash_node_t *new_node;
+        unsigned long int h_val;
+        const char *my_key;
+        const char *my_value;
+
+        h_val = hash_djb2((unsigned char *)key) % ht->size;
+        my_key = key;
+        my_value = value;
+
+        new_node = malloc(sizeof(hash_node_t));
+        if (new_node == NULL)
+        {
+                return (0);
+        }
+        new_node->key = (char *)my_key;
+        new_node->value = (char *)my_value;
+        new_node->next = ht->array[h_val + 1];
+
+        ht->array[h_val] = new_node;
+
+        return (1);
+}
