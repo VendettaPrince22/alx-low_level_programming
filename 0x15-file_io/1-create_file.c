0704820a672fd25df73ac90eabfc3286714885e3
Kairo Prince <pomegah@gmail.com>
2024-07-16 11:12:46 -0400

    Create a file, truncate it and stuff

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,7 @@
 /**
  * create_file - creates a file
  * @filename: name of the file to create
- * @text_content: string to write to the file
+ * @text_content: string to write to the file, NULL terminated
  * 
  * Return: 1 on success
  * On failure, return -1 (file cannot be created, files cannot be written)
@@ -30,6 +30,10 @@ int create_file(const char *filename, char *text_content)
         }
 
         fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+        if (fd < 0)
+        {
+                return (-1);
+        }
 
         if (text_content == NULL)
         {
