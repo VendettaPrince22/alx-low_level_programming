a1d8976a4844734badafb9dd3c6752a401a99fa9
Kairo Prince <pomegah@gmail.com>
2024-08-16 08:54:36 -0400

    Times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the 'n' times table
+ * @n: the number for which the times table prints
+ *
+ * If 'n' is greater than 15 or less than 0,
+ * the function shouldn't print anything
+ */
+void print_times_table(int n)
+{
+	int count;
+	int num;
+	int row;
+
+	if (!(n > 15 || n < 0))
+	{
+		count = 0;
+		while (count <= n)
+		{
+			num = 1;
+			row = 0;
+			printf("%d, ", row);
+			while (num < n)
+			{
+				row += count;
+				printf("%3d, ", row);
+				num++;
+			}
+			printf("%3d\n", row + count);
+			count++;
+		}
+	}
+}
